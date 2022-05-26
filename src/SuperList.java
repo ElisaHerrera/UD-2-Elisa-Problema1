c4dd52575df458247920b122ef81ba9dfd404af3
ElisaHerrera <102582595+ElisaHerrera@users.noreply.github.com>
2022-05-26 11:34:48 +0100

    problema  1

--- src/SuperList.java
+++ src/SuperList.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class SuperList {
+import java.util.Iterator;
+
+public class SuperList extends ListManager<Integer>{
+
+    @Override
+    public void addElement(Integer element) {
+       list.add(element);
+    }
+
+    @Override
+    public Integer removeElement(int position) {
+        list.remove(position);
+        for (int i = 0; i <removeElement(1) ; i++) {
+            Integer valor=removeElement(1);
+            System.out.println(valor);
+        }
+        return null;
+    }
+
+    @Override
+    public void showElements() {
+        Iterator<Integer> it = list.iterator();
+
+    }
+
+    @Override
+    public void showReversedElements() {
+
+    }
 }
